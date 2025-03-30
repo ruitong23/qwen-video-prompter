a2c8e6957cbfcd45b43b92970c76ea493b650e82
ruitong23 <ruitong23@gmail.com>
2025-03-30 19:34:50 -0400

    Added submodule, updated install/run scripts, enabled submodule loading

--- install.bat
+++ install.bat
@@ -1,16 +1,19 @@
 @echo off
 cd /d %~dp0
 
-echo [1/4] Creating virtual environment...
+echo [1/5] Creating virtual environment...
 python -m venv venv
 
-echo [2/4] Activating virtual environment...
+echo [2/5] Activating virtual environment...
 call "%~dp0venv\Scripts\activate.bat"
 
-echo [3/4] Installing requirements...
+echo [3/5] Installing Python dependencies...
 "%~dp0venv\Scripts\pip.exe" install --upgrade pip
 "%~dp0venv\Scripts\pip.exe" install -r requirements.txt
 
-echo [4/4] Setup complete!
+echo [4/5] Initializing Qwen2.5-VL submodule...
+git submodule update --init --recursive
+
+echo [5/5] Setup complete!
 pause
 exit /b
